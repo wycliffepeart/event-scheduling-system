7891260adf40c029a6e66d9f6a9cce18fa391bd1
Wycliffe Peart <wycliffepeart@users.noreply.github.com>
2025-03-27 18:06:20 -0500

    Remove unused methods from InvoiceRepository and rename file
    Cleaned up unused query methods in `InvoiceRepository` to improve maintainability and reduce clutter. Additionally, a file was renamed for better organization and clarity.

--- ess-server/src/main/java/com/ess/essserver/module/invoice/InvoiceRepository.java
+++ ess-server/src/main/java/com/ess/essserver/module/invoice/InvoiceRepository.java
@@ -1,19 +1,8 @@
 package com.ess.essserver.module.invoice;
 
-import com.ess.essserver.module.booking.BookingEntity;
-import com.ess.essserver.module.event.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface InvoiceRepository extends JpaRepository<InvoiceEntity, Long> {
-    List<InvoiceEntity> findAllByEvent(EventEntity event);
-
-    Optional<InvoiceEntity> findByEventAndId(EventEntity event, Long id);
-
-    Optional<InvoiceEntity> findByEventAndBookingsIn(EventEntity event, Collection<List<BookingEntity>> bookings);
 }
